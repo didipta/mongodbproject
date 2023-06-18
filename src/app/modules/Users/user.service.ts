baffc044ead0dfd15089d9cbc765cf03b331642d
didipta <66532706+didipta@users.noreply.github.com>
2023-06-18 11:37:26 +0600

    user service ad

--- src/app/modules/Users/user.service.ts
+++ src/app/modules/Users/user.service.ts
@@ -1,3 +1,6 @@
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../shared/pagination copy';
+import { paginationHelpers } from '../../../shared/paginationHelper';
 import { IUser } from './User.interface';
 import { User } from './User.model';
 
@@ -6,6 +9,50 @@ const createUser = async (payload: IUser): Promise<IUser | null> => {
   return result;
 };
 
+const getAlluser = async (
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IUser[]>> => {
+  const { limit, page, skip } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const result = await User.find({}).skip(skip).limit(limit);
+
+  const total = await User.countDocuments();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const getSingleuser = async (id: string): Promise<IUser | null> => {
+  const result = await User.findById(id);
+  return result;
+};
+
+const updateuser = async (
+  id: string,
+  payload: Partial<IUser>
+): Promise<IUser | null> => {
+  const result = await User.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
+
+const deleteuser = async (id: string): Promise<IUser | null> => {
+  const result = await User.findByIdAndDelete(id);
+  return result;
+};
+
 export const UserService = {
   createUser,
+  getSingleuser,
+  updateuser,
+  deleteuser,
+  getAlluser,
 };
