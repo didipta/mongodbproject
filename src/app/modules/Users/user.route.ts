46c7440e9b31dabcb8037f497558c1a3dcebb8c1
didipta <66532706+didipta@users.noreply.github.com>
2023-07-03 06:58:35 +0600

    role base auth

--- src/app/modules/Users/user.route.ts
+++ src/app/modules/Users/user.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import uservalidation from './uservalidation';
 import validateRequest from '../../middleware/validateRequest';
 import { UserController } from './user.controller';
+import auth from '../../middleware/auth';
+import { ENUM_USER_ROLE } from '../../../roleenum';
 const router = express.Router();
 
 router.post(
@@ -9,9 +11,9 @@ router.post(
   validateRequest(uservalidation),
   UserController.createUser
 );
-router.get('/', UserController.getAlluser);
-router.get('/:id', UserController.getSingleuser);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAlluser);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleuser);
 router.patch('/:id', UserController.updateuser);
-router.delete('/:id', UserController.deleteuser);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteuser);
 
 export const UserRoutes = router;
